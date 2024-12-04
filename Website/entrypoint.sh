a92277e9f947bcf38d6d4ea3537dcdc9d6d4590f
erippey3 <erippey3@vt.edu>
2024-12-04 13:25:49 -0500

    added migration as an option to main container

--- Website/entrypoint.sh
+++ Website/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Function to wait for the database to be ready using pg_isready
 wait_for_db() {
   echo "Waiting for postgres..."
   until pg_isready -h db -p 5432 -U postgres; do
@@ -13,13 +11,14 @@ wait_for_db() {
   echo "Postgres is up - continuing..."
 }
 
-# Wait for the database
 wait_for_db
 
-# Run database migrations
-echo "Running database migrations..."
-flask db upgrade
 
-# Start the Flask application
+# Check if migrations should be run
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+  echo "Running database migrations..."
+  flask db upgrade
+fi
+
 echo "Starting the Flask application..."
 exec gunicorn -b :5000 --access-logfile - --error-logfile - manage:app
--- Website/src/config.py
+++ Website/src/config.py
@@ -1,19 +1,51 @@
 import os
 from dotenv import load_dotenv
+import redis
+from datetime import timedelta
 
 # Load environment variables from the .env file
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = os.getenv('SECRET_KEY', 'here_is_the_key_for_our_project')  
-    UPLOAD_FOLDER = os.path.abspath(os.environ.get('REPO_DIR'))
-    SESSION_COOKIE_SECURE=True  # Only send cookie over HTTPS
-    SESSION_COOKIE_HTTPONLY=True  # Prevent JavaScript access to cookie
-    SESSION_COOKIE_SAMESITE='Lax'  # Protect against CSRF
+
+    # general server info
     SERVER_NAME= os.getenv('SERVER_NAME', '192.168.44.4:54003')
     PREFERRED_URL_SCHEME = os.getenv('PREFERRED_URL_SCHEME', 'http')
+    OATHLIB_INSECURE_TRANSPORT = os.getenv('OATHLIB_INSECURE_TRANSPORT', '0')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'here_is_the_key_for_our_project')  
+    UPLOAD_FOLDER = os.path.abspath(os.environ.get('REPO_DIR'))
+
+    # database management
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+    #session settings
+    SESSION_TYPE= 'redis'
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_KEY_PREFIX = 'session:'
+    SESSION_REDIS = redis.from_url('redis://redis:6379/0')
+    SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE', 'False').lower() in ('true', '1', 'yes')
+    SESSION_COOKIE_HTTPONLY=True
+    SESSION_COOKIE_SAMESITE= None
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+
+
+    OAUTH2_PROVIDERS = {
+        'github': {
+            'client_id': os.environ.get('GITHUB_CLIENT_ID'),
+            'client_secret': os.environ.get('GITHUB_CLIENT_SECRET'),
+            'authorize_url': 'https://github.com/login/oauth/authorize',
+            'token_url': 'https://github.com/login/oauth/access_token',
+            'userinfo': {
+                'email_url': 'https://api.github.com/user/emails',
+                'profile_url': 'https://api.github.com/user',
+            },
+            'scopes': ['user:email', 'read:user'],
+        },
+        # potentially add google oath later
+    }
 
 
     ALLOWED_EXTENSIONS = {
